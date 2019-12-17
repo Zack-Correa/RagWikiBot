3ae0932f9f686958768826225f1457ea49a39d56
Zack-Correa <zackluthier@hotmail.com>
2019-12-17 14:14:53 -0300

    initial commit

---
+++ integrations/bropedia/bropediaRequest.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const settings = require('./integrations/const.json');
+
+var endpoint = settings.endpoints[0].bropediaUrl;
+var actionParam = settings.endpoints[0].params.action;
+
+
+function makeRequest(keyword) {
+    var options = {method: 'GET', url: endpoint, qs: {action: actionParam, search: keyword}};
+    request(options, (error, response, body) => {
+        if(error) {
+            console.log(error);
+            return null;
+        }
+        return body;
+    })
+}
