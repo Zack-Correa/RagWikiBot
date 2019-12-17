cfccd133949ca2a728075cba9c37c25f3a4d890c
Zack-Correa <zackluthier@hotmail.com>
2019-12-17 16:02:56 -0300

    minor json changes

--- integrations/bropedia/bropediaRequest.js
+++ integrations/bropedia/bropediaRequest.js
@@ -1,12 +1,13 @@
 const request = require("request");
 const settings = require('./integrations/const.json');
 
-var endpoint = settings.endpoints[0].bropediaUrl;
+var endpoint = settings.endpoints[0].url;
 var actionParam = settings.endpoints[0].params.action;
 
 
 function makeRequest(keyword) {
     var options = {method: 'GET', url: endpoint, qs: {action: actionParam, search: keyword}};
+    
     request(options, (error, response, body) => {
         if(error) {
             console.log(error);
